abc4e65f808e3c60e48ae8d155c2f3dbd2af8b59
kyoons1211 <kyoons1211@gmail.com>
2025-12-09 10:10:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ second = input("Enter the second number: ")
 num1 = int(first)
 num2 = int(second)
 
-result = num1 + num2
+result = num1 - num2
 
-print("Sum: ", result)
+print("Difference: ", result)
